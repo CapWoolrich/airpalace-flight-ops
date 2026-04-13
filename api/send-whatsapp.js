8c93363fdd4042c8bb014d657989544578e5bd38
CapWoolrich <bernardwoolrich@gmail.com>
2026-04-13 06:54:52 -0600

    Update send-whatsapp.js

--- api/send-whatsapp.js
+++ api/send-whatsapp.js
@@ -1,4 +1,4 @@
-import { buildWhatsAppFlightMessage } from "./_whatsappMessage";
+import { buildWhatsAppFlightMessage } from "./_whatsappMessage.js";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
